8e956846e21f07e2013b26d4717c468b9a8fb6c2
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-08-19 22:01:34 +0200

    Added routes for screens in Main screen alongside bottom app bar

--- app/src/main/java/com/puzzle/industries/budgetplan/navigation/constants/Routes.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/navigation/constants/Routes.kt
@@ -5,6 +5,11 @@ open class Routes(val path: String) {
     object Splash: Routes(MainScreens.Splash.name)
     object Main: Routes(MainScreens.Main.name)
 
+    //home nested screens
+    object Home: Routes(HomeScreens.Home.name)
+    object Budget: Routes(HomeScreens.Budget.name)
+    object Search: Routes(HomeScreens.Search.name)
+
     fun addParam(key: String, value: String) : Routes{
         val newPath = path.replace("{$key}", value)
         return Routes(newPath)
