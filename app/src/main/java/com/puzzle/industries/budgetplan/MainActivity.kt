c219b31e60679e47a3619fe2ddba669a8b94fccd
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-08-19 22:02:46 +0200

    modified navHost

--- app/src/main/java/com/puzzle/industries/budgetplan/MainActivity.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/MainActivity.kt
@@ -3,14 +3,16 @@ package com.puzzle.industries.budgetplan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import com.puzzle.industries.budgetplan.navigation.MainNavigation
+import com.puzzle.industries.budgetplan.navigation.AppScreensNavHost
 import com.puzzle.industries.budgetplan.theme.BudgetPlanTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
+@ExperimentalMaterial3Api
 @ExperimentalMaterial3WindowSizeClassApi
 class MainActivity : ComponentActivity() {
 
@@ -18,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BudgetPlanTheme(windowSizeClass = calculateWindowSizeClass(activity = this)) {
-                MainNavigation()
+                AppScreensNavHost()
             }
         }
     }
