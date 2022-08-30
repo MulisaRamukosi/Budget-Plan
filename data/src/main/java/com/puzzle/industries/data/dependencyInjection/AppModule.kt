fe403ae5264105610b98f45473663fad637f4f81
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-08-30 21:31:49 +0200

    setup dependency injection for database and dao

--- data/src/main/java/com/puzzle/industries/data/dependencyInjection/AppModule.kt
+++ data/src/main/java/com/puzzle/industries/data/dependencyInjection/AppModule.kt
@@ -1,10 +1,20 @@
 package com.puzzle.industries.data.dependencyInjection
 
+import android.content.Context
+import com.puzzle.industries.data.database.AppDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase =
+        AppDatabase.buildDatabase(context = context)
 }
---
+++ data/src/main/java/com/puzzle/industries/data/dependencyInjection/DaoModule.kt
@@ -0,0 +1,46 @@
+package com.puzzle.industries.data.dependencyInjection
+
+import com.puzzle.industries.data.database.AppDatabase
+import com.puzzle.industries.data.database.dao.expense.ExpenseDao
+import com.puzzle.industries.data.database.dao.expense.ExpenseHistoryDao
+import com.puzzle.industries.data.database.dao.expenseGroup.ExpenseGroupDao
+import com.puzzle.industries.data.database.dao.expenseGroup.ExpenseGroupHistoryDao
+import com.puzzle.industries.data.database.dao.income.IncomeDao
+import com.puzzle.industries.data.database.dao.income.IncomeHistoryDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DaoModule {
+
+    @Singleton
+    @Provides
+    fun provideIncomeDao(database: AppDatabase): IncomeDao = database.incomeDao()
+
+    @Singleton
+    @Provides
+    fun provideIncomeHistoryDao(database: AppDatabase): IncomeHistoryDao =
+        database.incomeHistoryDao()
+
+    @Singleton
+    @Provides
+    fun provideExpenseDao(database: AppDatabase): ExpenseDao = database.expenseDao()
+
+    @Singleton
+    @Provides
+    fun provideExpenseHistoryDao(database: AppDatabase): ExpenseHistoryDao =
+        database.expenseHistoryDao()
+
+    @Singleton
+    @Provides
+    fun provideExpenseGroupDao(database: AppDatabase): ExpenseGroupDao = database.expenseGroupDao()
+
+    @Singleton
+    @Provides
+    fun provideExpenseGroupHistoryDao(database: AppDatabase): ExpenseGroupHistoryDao =
+        database.expenseGroupHistoryDao()
+}
