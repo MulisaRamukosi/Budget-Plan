4967e04fb7b32739d3c5460ba7c0b0cd2958fbdf
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-08-30 17:04:50 +0200

    renamed interface

--- domain/src/main/java/com/puzzle/industries/domain/repository/income/IncomeRepository.kt
+++ domain/src/main/java/com/puzzle/industries/domain/repository/income/IncomeRepository.kt
@@ -3,7 +3,7 @@ package com.puzzle.industries.domain.repository.income
 import com.puzzle.industries.domain.models.income.Income
 import com.puzzle.industries.domain.common.Create
 import com.puzzle.industries.domain.common.Delete
-import com.puzzle.industries.domain.common.ReadAll
+import com.puzzle.industries.domain.common.Read
 import com.puzzle.industries.domain.common.Update
 
-interface IncomeRepository : Create<Income>, ReadAll<Income>, Update<Income>, Delete<String>
+interface IncomeRepository : Create<Income>, Read<Income>, Update<Income>, Delete<String>
