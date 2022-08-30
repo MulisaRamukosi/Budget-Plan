6214ea7695a58a5a2f4b9428694268c0b2e6a411
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-08-30 17:04:20 +0200

    removed redundant interface reference

--- domain/src/main/java/com/puzzle/industries/domain/repository/expense/ExpenseRepository.kt
+++ domain/src/main/java/com/puzzle/industries/domain/repository/expense/ExpenseRepository.kt
@@ -3,7 +3,6 @@ package com.puzzle.industries.domain.repository.expense
 import com.puzzle.industries.domain.models.expense.Expense
 import com.puzzle.industries.domain.common.Create
 import com.puzzle.industries.domain.common.Delete
-import com.puzzle.industries.domain.common.MultiDelete
 import com.puzzle.industries.domain.common.Update
 
-interface ExpenseRepository : Create<Expense>, Update<Expense>, Delete<Expense>, MultiDelete<Expense>
+interface ExpenseRepository : Create<Expense>, Update<Expense>, Delete<Expense>
