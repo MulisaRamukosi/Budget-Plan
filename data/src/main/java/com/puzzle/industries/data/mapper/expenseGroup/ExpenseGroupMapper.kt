c65d1f45e4843126a9533ee2821d97ef15bfebf6
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-08-31 18:16:13 +0200

    added a mapper function

--- data/src/main/java/com/puzzle/industries/data/mapper/expenseGroup/ExpenseGroupMapper.kt
+++ data/src/main/java/com/puzzle/industries/data/mapper/expenseGroup/ExpenseGroupMapper.kt
@@ -1,7 +1,11 @@
 package com.puzzle.industries.data.mapper.expenseGroup
 
 import com.puzzle.industries.data.database.entity.expenseGroup.ExpenseGroupEntity
+import com.puzzle.industries.data.database.entity.expenseGroup.ExpenseGroupWithExpensesEntity
+import com.puzzle.industries.domain.constants.Frequency
+import com.puzzle.industries.domain.models.expense.Expense
 import com.puzzle.industries.domain.models.expenseGroup.ExpenseGroup
+import com.puzzle.industries.domain.models.expenseGroup.ExpenseGroupWithExpenses
 
 internal class ExpenseGroupMapper {
 
@@ -12,7 +16,22 @@ internal class ExpenseGroupMapper {
         )
     }
 
-    fun toExpenseGroup(expenseGroup: ExpenseGroupEntity): ExpenseGroup {
+    fun toExpenseGroupWithExpenses(entity: ExpenseGroupWithExpensesEntity): ExpenseGroupWithExpenses {
+        return ExpenseGroupWithExpenses(
+            expenseGroup = toExpenseGroup(entity.expenseGroup),
+            expenses = entity.expenses.map { expenseEntity ->
+                Expense(
+                    id = expenseEntity.id,
+                    expenseGroupId = expenseEntity.expenseGroupId,
+                    name = expenseEntity.name,
+                    amount = expenseEntity.amount,
+                    frequency = Frequency.valueOf(expenseEntity.frequency)
+                )
+            }
+        )
+    }
+
+    private fun toExpenseGroup(expenseGroup: ExpenseGroupEntity): ExpenseGroup {
         return ExpenseGroup(
             id = expenseGroup.id,
             name = expenseGroup.name,
