05026fbef3641efeb24c2c9a665a42c2340c8ad4
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-03 11:06:30 +0200

    modified model to have default values

--- domain/src/main/java/com/puzzle/industries/domain/models/income/Income.kt
+++ domain/src/main/java/com/puzzle/industries/domain/models/income/Income.kt
@@ -4,10 +4,10 @@ import com.puzzle.industries.domain.constants.Frequency
 import java.util.*
 
 data class Income(
-    val id: UUID,
+    val id: UUID = UUID.randomUUID(),
     val frequency: Frequency,
     val amount: Double,
     val title: String,
     val description: String,
-    val lastModifyDate: Date
+    val lastModifyDate: Date = Date()
 )
