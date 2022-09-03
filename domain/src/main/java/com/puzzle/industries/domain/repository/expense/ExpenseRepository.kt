d013feba8668d71b32d0323f2490f466c3f4a1d9
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-03 11:10:38 +0200

    imports refactored due to moved files

--- domain/src/main/java/com/puzzle/industries/domain/repository/expense/ExpenseRepository.kt
+++ domain/src/main/java/com/puzzle/industries/domain/repository/expense/ExpenseRepository.kt
@@ -1,8 +1,6 @@
 package com.puzzle.industries.domain.repository.expense
 
 import com.puzzle.industries.domain.models.expense.Expense
-import com.puzzle.industries.domain.common.Create
-import com.puzzle.industries.domain.common.Delete
-import com.puzzle.industries.domain.common.Update
+import com.puzzle.industries.domain.common.crud.*
 
 interface ExpenseRepository : Create<Expense>, Update<Expense>, Delete<Expense>
--- domain/src/main/java/com/puzzle/industries/domain/repository/expenseGroup/ExpenseGroupRepository.kt
+++ domain/src/main/java/com/puzzle/industries/domain/repository/expenseGroup/ExpenseGroupRepository.kt
@@ -1,6 +1,6 @@
 package com.puzzle.industries.domain.repository.expenseGroup
 
-import com.puzzle.industries.domain.common.*
+import com.puzzle.industries.domain.common.crud.*
 import com.puzzle.industries.domain.models.expenseGroup.ExpenseGroup
 import com.puzzle.industries.domain.models.expenseGroup.ExpenseGroupWithExpenses
 
