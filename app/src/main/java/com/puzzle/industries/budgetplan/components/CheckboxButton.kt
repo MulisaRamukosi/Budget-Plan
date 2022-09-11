ac7c8d002f2b4db02e3fcfd3e63b3ce2d098f4a5
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-11 23:00:16 +0200

    created checkbox component

---
+++ app/src/main/java/com/puzzle/industries/budgetplan/components/CheckboxButton.kt
@@ -0,0 +1,33 @@
+package com.puzzle.industries.budgetplan.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.puzzle.industries.budgetplan.theme.spacing
+
+@Composable
+fun CheckboxButton(
+    modifier: Modifier = Modifier,
+    checked: Boolean,
+    onCheckChanged: (Boolean) -> Unit,
+    content: @Composable () -> Unit
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = checked, onCheckedChange = onCheckChanged)
+        content()
+        Spacer(modifier = Modifier.width(width = MaterialTheme.spacing.large))
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewCheckboxButton(){
+    CheckboxButton(modifier = Modifier,  checked = false, onCheckChanged = {}) {
+        Text(text = "preview text")
+    }
+}
