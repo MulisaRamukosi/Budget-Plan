a89ba8d69ac2f219d55365e53b84d17756a956ce
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-11 23:31:51 +0200

    created single line component

---
+++ app/src/main/java/com/puzzle/industries/budgetplan/components/SingleLineText.kt
@@ -0,0 +1,26 @@
+package com.puzzle.industries.budgetplan.components
+
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+
+@Composable
+fun SingleLineText(
+    modifier: Modifier = Modifier,
+    text: String,
+    color: Color = Color.Unspecified,
+    style: TextStyle = LocalTextStyle.current
+) {
+    Text(
+        modifier = modifier,
+        text = text,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        color = color,
+        style = style
+    )
+}
