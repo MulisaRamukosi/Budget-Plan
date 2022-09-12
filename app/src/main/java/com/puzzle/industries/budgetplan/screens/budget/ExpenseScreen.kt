adb8b9890925dfd444ffa1b45e15ab7804454581
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-12 22:11:40 +0200

    initial work

--- app/src/main/java/com/puzzle/industries/budgetplan/screens/budget/ExpenseScreen.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/screens/budget/ExpenseScreen.kt
@@ -1,19 +1,41 @@
 package com.puzzle.industries.budgetplan.screens.budget
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.puzzle.industries.budgetplan.components.expenses.ExpenseGroupItem
+import com.puzzle.industries.budgetplan.theme.BudgetPlanTheme
 
+@ExperimentalAnimationApi
 @Composable
 fun ExpenseScreen(){
-    Column(Modifier.fillMaxSize()){
-        Text(text ="efd")
+    Column(
+        Modifier
+            .padding(all = 16.dp)
+            .fillMaxSize()){
+        repeat(5){
+            ExpenseGroupItem()
+            Spacer(modifier = Modifier.height(height = 16.dp))
+        }
     }
 }
 
 @Composable
 private fun Content(){
 
+}
+
+@Composable
+@Preview(showBackground = true)
+@ExperimentalAnimationApi
+@ExperimentalMaterial3WindowSizeClassApi
+private fun ExpenseScreenPreview(){
+    BudgetPlanTheme(dynamicColor = false) {
+        ExpenseScreen()
+    }
 }
