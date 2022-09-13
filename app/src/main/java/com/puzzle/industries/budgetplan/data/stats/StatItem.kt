3d851a7594c96a1964a73aea7fc4136db198a2ec
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-13 21:46:50 +0200

    created model to use in stats

---
+++ app/src/main/java/com/puzzle/industries/budgetplan/data/stats/StatItem.kt
@@ -0,0 +1,9 @@
+package com.puzzle.industries.budgetplan.data.stats
+
+import androidx.compose.ui.graphics.Color
+
+data class StatItem(
+    val title: String,
+    val value: Double,
+    val color: Color
+)
