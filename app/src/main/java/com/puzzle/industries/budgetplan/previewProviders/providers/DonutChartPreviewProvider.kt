3352326ae0e2d218a1cfcc7723598676a7162f63
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-14 19:33:39 +0200

    migrated to using key for title and value

--- app/src/main/java/com/puzzle/industries/budgetplan/previewProviders/providers/DonutChartPreviewProvider.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/previewProviders/providers/DonutChartPreviewProvider.kt
@@ -2,23 +2,24 @@ package com.puzzle.industries.budgetplan.previewProviders.providers
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.puzzle.industries.budgetplan.data.stats.Key
 import com.puzzle.industries.budgetplan.data.stats.StatItem
 
 class DonutChartPreviewProvider : PreviewParameterProvider<List<StatItem>> {
     override val values: Sequence<List<StatItem>>
         get() = sequenceOf(
             listOf(
-                StatItem(title = "Entertainment", value = 70.0, color = Color(color = 0xFF103D61)),
-                StatItem(title = "Groceries", value = 20.0, color = Color(color = 0xFF1F7947)),
-                StatItem(title = "HouseHold", value = 40.0, color = Color(color = 0xFF9E9333)),
-                StatItem(title = "Family", value = 40.0, color = Color(color = 0xFFA74343)),
+                StatItem(key = Key(title = "Entertainment", color = Color(color = 0xFF103D61)), value = 70.0),
+                StatItem(key = Key(title = "Groceries", color = Color(color = 0xFF1F7947)), value = 20.0),
+                StatItem(key = Key(title = "HouseHold", color = Color(color = 0xFF9E9333)), value = 40.0),
+                StatItem(key = Key(title = "Family", color = Color(color = 0xFFA74343)), value = 40.0),
             ),
             listOf(
-                StatItem(title = "Entertainment", value = 70.0, color = Color(color = 0xFF103D61)),
-                StatItem(title = "Groceries", value = 20.0, color = Color(color = 0xFF1F7947))
+                StatItem(key = Key(title = "Entertainment", color = Color(color = 0xFF103D61)), value = 70.0),
+                StatItem(key = Key(title = "Groceries", color = Color(color = 0xFF1F7947)), value = 20.0)
             ),
             listOf(
-                StatItem(title = "Entertainment", value = 70.0, color = Color(color = 0xFF103D61))
+                StatItem(key = Key(title = "Entertainment", color = Color(color = 0xFF103D61)), value = 70.0)
             )
         )
 }
