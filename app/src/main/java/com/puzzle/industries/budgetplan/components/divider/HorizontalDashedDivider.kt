1918321e2178c23dd8f564f5d1f636ef9b31c4c4
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-14 19:43:20 +0200

    created horizontal dashed Divider

---
+++ app/src/main/java/com/puzzle/industries/budgetplan/components/divider/HorizontalDashedDivider.kt
@@ -0,0 +1,29 @@
+package com.puzzle.industries.budgetplan.components.divider
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HorizontalDashedDivider(
+    modifier: Modifier,
+    thickness: Dp = 1.dp,
+    color: Color = MaterialTheme.colorScheme.outline
+) {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    Canvas(modifier = modifier.height(height = thickness)) {
+        drawLine(
+            color = color,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect
+        )
+    }
+}
