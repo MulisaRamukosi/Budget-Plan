3356e1174294cd729bcfb6a7ab74799e52e425b0
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-14 19:34:06 +0200

    migrated to using key for title and value

--- app/src/main/java/com/puzzle/industries/budgetplan/data/stats/StatItem.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/data/stats/StatItem.kt
@@ -3,7 +3,6 @@ package com.puzzle.industries.budgetplan.data.stats
 import androidx.compose.ui.graphics.Color
 
 data class StatItem(
-    val title: String,
-    val value: Double,
-    val color: Color
+    val key: Key,
+    val value: Double
 )
