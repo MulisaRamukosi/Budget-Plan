b918ecfae767e0ad56920fa855362072a982ec8e
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-16 09:52:14 +0200

    optimized UI

--- app/src/main/java/com/puzzle/industries/budgetplan/screens/intro/registrationGuideFlow/GuideFlowComponents.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/screens/intro/registrationGuideFlow/GuideFlowComponents.kt
@@ -1,8 +1,6 @@
 package com.puzzle.industries.budgetplan.screens.intro.registrationGuideFlow
 
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material3.Button
@@ -11,12 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.puzzle.industries.budgetplan.R
+import com.puzzle.industries.budgetplan.components.spacer.H_S_Space
 import com.puzzle.industries.budgetplan.components.spacer.V_M_Space
 import com.puzzle.industries.budgetplan.components.spacer.V_S_Space
-import com.puzzle.industries.budgetplan.theme.spacing
 
 object GuideFlowComponents {
     @Composable
@@ -48,7 +45,9 @@ object GuideFlowComponents {
         Button(onClick = onContinueClick) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(text = stringResource(id = R.string.opt_continue))
-                Spacer(modifier = Modifier.width(width = MaterialTheme.spacing.small))
+
+                H_S_Space()
+
                 Icon(
                     imageVector = Icons.Rounded.ChevronRight,
                     contentDescription = stringResource(id = R.string.desc_arrow_right_icon)
