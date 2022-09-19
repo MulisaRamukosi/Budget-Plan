e4bbbaa7181dc9b85781760e347f613c07e60610
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-19 21:58:53 +0200

    ditched previous implementation of clearing stack

--- app/src/main/java/com/puzzle/industries/budgetplan/navigation/graphs/ext/extensions.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/navigation/graphs/ext/extensions.kt
@@ -1,12 +1,9 @@
 package com.puzzle.industries.budgetplan.navigation.graphs.ext
 
 import androidx.navigation.NavHostController
-import androidx.navigation.NavOptionsBuilder
 
-fun NavOptionsBuilder.clearAllPreviousRoutes(navController: NavHostController) {
-    navController.graph.startDestinationRoute?.let {
-        popUpTo(route = it) {
-            inclusive = true
-        }
-    }
+fun NavHostController.navigateAndClearStack(route: String) {
+    popBackStack(graph.startDestinationId, true)
+    graph.setStartDestination(startDestRoute = route)
+    navigate(route = route)
 }
