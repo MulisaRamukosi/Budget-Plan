d9c635e435d9bf8d4d7ed25511a2bd8d1b2681f2
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-19 21:55:11 +0200

    renamed preference service

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/SplashScreenViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/SplashScreenViewModel.kt
@@ -1,15 +1,15 @@
 package com.puzzle.industries.budgetplan.viewModels
 
 import androidx.lifecycle.ViewModel
-import com.puzzle.industries.domain.services.launch.LaunchTrackingService
+import com.puzzle.industries.domain.services.LaunchTrackingPreferenceService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashScreenViewModel @Inject constructor(private val launchTrackingService: LaunchTrackingService)
-    : ViewModel() {
+class SplashScreenViewModel @Inject constructor(private val launchTrackingPreferenceService: LaunchTrackingPreferenceService) :
+    ViewModel() {
 
-        fun isFirstTimeLaunch(): Boolean {
-            return launchTrackingService.isFirstTimeLaunch()
-        }
+    fun isFirstTimeLaunch(): Boolean {
+        return launchTrackingPreferenceService.isFirstTimeLaunch()
+    }
 }
