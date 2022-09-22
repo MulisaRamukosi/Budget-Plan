0df409ca9a8a2b29591f6e637183c544207a73b9
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-22 22:22:19 +0200

    removed redundant widthSizeClass variable

--- app/src/main/java/com/puzzle/industries/budgetplan/screens/WelcomeScreen.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/screens/WelcomeScreen.kt
@@ -15,14 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -51,7 +49,6 @@ fun WelcomeScreen(
             currentWelcomeMessage = currentWelcomeMessage,
             selectedIndex = currentPage,
             numOfPages = numOfPages,
-            widthSizeClass = windowSizeClass.widthSizeClass,
             heightSizeClass = windowSizeClass.heightSizeClass,
             onNextClick = onNextClick
         )
@@ -64,7 +61,6 @@ private fun WelcomeMessageSection(
     currentWelcomeMessage: WelcomeMessage,
     selectedIndex: Int,
     numOfPages: Int,
-    widthSizeClass: WindowWidthSizeClass,
     heightSizeClass: WindowHeightSizeClass,
     onNextClick: () -> Unit
 ) {
