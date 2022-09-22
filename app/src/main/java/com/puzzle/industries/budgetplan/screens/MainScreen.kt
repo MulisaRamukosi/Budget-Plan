bfceff948db89241cdb669975e7ebb76f61bdd2c
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-22 22:21:38 +0200

    switched over to screen aware scaffold

--- app/src/main/java/com/puzzle/industries/budgetplan/screens/MainScreen.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/screens/MainScreen.kt
@@ -1,6 +1,5 @@
 @file:OptIn(
-    ExperimentalMaterial3WindowSizeClassApi::class,
-    ExperimentalMaterial3Api::class
+    ExperimentalMaterial3WindowSizeClassApi::class
 )
 
 package com.puzzle.industries.budgetplan.screens
@@ -11,12 +10,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountBalanceWallet
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.QueryStats
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,11 +21,10 @@ import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.puzzle.industries.budgetplan.R
+import com.puzzle.industries.budgetplan.components.ScreenSizeAwareScaffold
 import com.puzzle.industries.budgetplan.components.appBar.bottomAppBar.BottomAppBarActionButton
-import com.puzzle.industries.budgetplan.components.appBar.bottomAppBar.bottomAppBar
 import com.puzzle.industries.budgetplan.navigation.MainScreenNestedNavHost
 import com.puzzle.industries.budgetplan.navigation.constants.Routes
 import com.puzzle.industries.budgetplan.theme.BudgetPlanTheme
@@ -38,40 +34,32 @@ fun MainScreen(
     navController: NavHostController = rememberNavController(),
     windowSizeClass: WindowSizeClass
 ) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination?.route
+    ScreenSizeAwareScaffold(
+        windowSizeClass = windowSizeClass, actions = actions(
+            navController = navController
+        ), navController = navController
+    ) { paddingValues ->
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = bottomAppBar(
-            selectedRoute = currentDestination ?: Routes.Home.path,
-            actions = bottomAppBarActions(navController = navController)
+        MainScreenNestedNavHost(
+            modifier = Modifier.padding(paddingValues = paddingValues).fillMaxSize(),
+            navController = navController
         )
-    ) {
 
-        MainScreenNestedNavHost(navController = navController, modifier = Modifier.padding(it))
     }
 }
 
 @Composable
-private fun Content(
-    navController: NavHostController
-) {
-
-}
-
-@Composable
-private fun bottomAppBarActions(navController: NavHostController): List<BottomAppBarActionButton> {
+private fun actions(navController: NavHostController): List<BottomAppBarActionButton> {
     return listOf(
         BottomAppBarActionButton(
             imageVector = Icons.Rounded.Home,
             label = stringResource(id = R.string.home),
             description = stringResource(id = R.string.desc_home),
             destinationRoute = Routes.Home.path,
-            onActionClick = {
+            onActionClick = { route ->
                 bottomAppBarOnClick(
                     navController = navController,
-                    destinationRoute = Routes.Home.path
+                    destinationRoute = route
                 )
             }
         ),
@@ -80,10 +68,10 @@ private fun bottomAppBarActions(navController: NavHostController): List<BottomAp
             label = stringResource(id = R.string.budget),
             description = stringResource(id = R.string.desc_budget),
             destinationRoute = Routes.Budget.path,
-            onActionClick = {
+            onActionClick = { route ->
                 bottomAppBarOnClick(
                     navController = navController,
-                    destinationRoute = Routes.Budget.path
+                    destinationRoute = route
                 )
             }
         ),
@@ -92,10 +80,22 @@ private fun bottomAppBarActions(navController: NavHostController): List<BottomAp
             label = stringResource(id = R.string.search),
             description = stringResource(id = R.string.desc_search),
             destinationRoute = Routes.Search.path,
-            onActionClick = {
+            onActionClick = { route ->
                 bottomAppBarOnClick(
                     navController = navController,
-                    destinationRoute = Routes.Search.path
+                    destinationRoute = route
+                )
+            }
+        ),
+        BottomAppBarActionButton(
+            imageVector = Icons.Rounded.Settings,
+            label = stringResource(id = R.string.settings),
+            description = stringResource(id = R.string.desc_settings),
+            destinationRoute = Routes.Settings.path,
+            onActionClick = { route ->
+                bottomAppBarOnClick(
+                    navController = navController,
+                    destinationRoute = route
                 )
             }
         )
@@ -118,11 +118,13 @@ private fun bottomAppBarOnClick(navController: NavHostController, destinationRou
 @Preview(showBackground = true)
 private fun MainScreenPreview() {
     BudgetPlanTheme(dynamicColor = false) {
-        MainScreen(windowSizeClass = WindowSizeClass.calculateFromSize(
-            DpSize(
-                width = 500.dp,
-                height = 800.dp
+        MainScreen(
+            windowSizeClass = WindowSizeClass.calculateFromSize(
+                DpSize(
+                    width = 500.dp,
+                    height = 800.dp
+                )
             )
-        ))
+        )
     }
 }
