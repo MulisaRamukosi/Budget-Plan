83ca8ed6ba3776a2847133568053ff63a788a891
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-26 17:28:19 +0200

    code auto formatting

--- app/src/main/java/com/puzzle/industries/budgetplan/navigation/NavigationHosts.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/navigation/NavigationHosts.kt
@@ -1,35 +1,29 @@
-@file:OptIn(
-    ExperimentalPagerApi::class,
-    ExperimentalMaterial3Api::class,
-    ExperimentalAnimationApi::class
-)
-
 package com.puzzle.industries.budgetplan.navigation
 
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.pager.ExperimentalPagerApi
 import com.puzzle.industries.budgetplan.navigation.graphs.appScreensGraph
-import com.puzzle.industries.budgetplan.navigation.graphs.mainScreenGraph
+import com.puzzle.industries.budgetplan.navigation.graphs.homeScreenGraph
+import com.puzzle.industries.budgetplan.viewModels.budget.IncomeViewModel
 
 @Composable
 fun AppScreensNavHost(modifier: Modifier = Modifier, windowSizeClass: WindowSizeClass) {
     val navController = rememberNavController()
+    val graph = appScreensGraph(navController = navController, windowSizeClass = windowSizeClass)
     NavHost(
         modifier = modifier,
         navController = navController,
-        graph = appScreensGraph(navController = navController, windowSizeClass)
+        graph = graph
     )
 }
 
 @Composable
-fun MainScreenNestedNavHost(
+fun HomeScreenNestedNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController
 ){
