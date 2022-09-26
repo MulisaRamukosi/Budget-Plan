e5127fee8981ee5df240e95d1eb1f4441f369d02
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-26 17:23:26 +0200

    side effect of moving file

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/registrationFlow/CurrencyViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/registrationFlow/CurrencyViewModel.kt
@@ -1,11 +1,8 @@
 package com.puzzle.industries.budgetplan.viewModels.registrationFlow
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.puzzle.industries.budgetplan.R
 import com.puzzle.industries.budgetplan.data.CountryCurrency
-import com.puzzle.industries.budgetplan.viewModels.PubSubViewModel
+import com.puzzle.industries.budgetplan.viewModels.custom.PubSubViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
