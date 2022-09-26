e703f9548f298934a4953e6da6037450628052f9
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-26 17:21:32 +0200

    code auto format

--- app/src/main/java/com/puzzle/industries/budgetplan/navigation/graphs/HomeScreenGraph.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/navigation/graphs/HomeScreenGraph.kt
@@ -1,38 +1,37 @@
 package com.puzzle.industries.budgetplan.navigation.graphs
 
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.createGraph
-import com.google.accompanist.pager.ExperimentalPagerApi
+import com.puzzle.industries.budgetplan.navigation.constants.RouteParamKey
 import com.puzzle.industries.budgetplan.navigation.constants.Routes
+import com.puzzle.industries.budgetplan.screens.home.BudgetScreen
 import com.puzzle.industries.budgetplan.screens.home.HomeScreen
 import com.puzzle.industries.budgetplan.screens.home.SearchScreen
-import com.puzzle.industries.budgetplan.screens.home.BudgetScreen
 import com.puzzle.industries.budgetplan.screens.home.SettingsScreen
+import com.puzzle.industries.budgetplan.viewModels.budget.IncomeViewModel
+import java.util.*
 
 @Composable
-@ExperimentalMaterial3Api
-@ExperimentalAnimationApi
-@ExperimentalPagerApi
-fun mainScreenGraph(navController: NavHostController) : NavGraph {
-    return navController.createGraph(startDestination = Routes.Home.path){
-        composable(route = Routes.Home.path){
-            HomeScreen()
+fun homeScreenGraph(
+    navController: NavHostController,
+    incomeViewModel: IncomeViewModel
+): NavGraph {
+    return navController.createGraph(startDestination = Routes.Home.path) {
+        composable(route = Routes.Home.path) {
+            HomeScreen(incomeViewModel = incomeViewModel)
         }
 
-        composable(route = Routes.Budget.path){
-            BudgetScreen()
-        }
+        budgetScreensGraph(navController = navController, incomeViewModel = incomeViewModel)
+        
 
-        composable(route = Routes.Search.path){
+        composable(route = Routes.Search.path) {
             SearchScreen()
         }
 
-        composable(route = Routes.Settings.path){
+        composable(route = Routes.Settings.path) {
             SettingsScreen()
         }
     }
