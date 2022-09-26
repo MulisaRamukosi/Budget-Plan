62f81e465dfdbe7e83140f553912f6fe1fa4d561
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-26 17:33:14 +0200

    extracted day picker

--- app/src/main/java/com/puzzle/industries/budgetplan/screens/registration/BudgetPlanGenerationDayScreen.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/screens/registration/BudgetPlanGenerationDayScreen.kt
@@ -1,11 +1,6 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.puzzle.industries.budgetplan.screens.registration
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Event
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
@@ -14,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.puzzle.industries.budgetplan.R
+import com.puzzle.industries.budgetplan.components.picker.MonthDayPicker
 import com.puzzle.industries.budgetplan.components.spacer.V_M_Space
 import com.puzzle.industries.budgetplan.theme.spacing
 import com.puzzle.industries.budgetplan.viewModels.registrationFlow.BudgetPlanDayViewModel
@@ -36,7 +32,7 @@ fun BudgetPlanGenerationDayScreen(
                 note = stringResource(id = R.string.note_changeable_in_settings)
             )
 
-            DaySelection(day = selectedDay){ day ->
+            MonthDayPicker(day = selectedDay){ day ->
                 viewModel.pub.value = day
             }
 
@@ -52,41 +48,3 @@ fun BudgetPlanGenerationDayScreen(
 
 }
 
-@Composable
-private fun DaySelection(day: Int, onClick: (Int) -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
-
-    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {expanded = !expanded}) {
-        OutlinedTextField(
-            modifier = Modifier.clickable { expanded = !expanded },
-            value = "$day",
-            onValueChange = {},
-            readOnly = true,
-            leadingIcon = {
-                Icon(
-                    imageVector = Icons.Rounded.Event,
-                    contentDescription = stringResource(id = R.string.desc_budget_plan_day_icon)
-                )
-            },
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-            },
-        )
-
-        ExposedDropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-        ) {
-            (1..31).forEach { day ->
-                DropdownMenuItem(
-                    text = { Text(text = day.toString()) },
-                    onClick = {
-                        onClick(day)
-                        expanded = false
-                    }
-                )
-            }
-        }
-    }
-
-}
