e39a1fbd0b7610b434201266e65ba51274967ef5
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-27 20:08:04 +0200

    added more converter functions

--- domain/src/main/java/com/puzzle/industries/domain/models/FrequencyDate.kt
+++ domain/src/main/java/com/puzzle/industries/domain/models/FrequencyDate.kt
@@ -6,22 +6,30 @@ data class FrequencyDate(
     val day: Int,
     val month: Int,
     val year: Int,
+) {
+    fun toDayMonthString(): String{
+        return Constants.DATE_FORMAT
+            .replace("dd", if(day < 10) "0$day" else day.toString())
+            .replace("MM", if(month < 10) "0$month" else month.toString())
+            .replace("/yyyy", "")
+    }
 
-
-    ) {
     override fun toString(): String {
         return Constants.DATE_FORMAT
-            .replace("dd", day.toString())
-            .replace("MM", month.toString())
+            .replace("dd", if(day < 10) "0$day" else day.toString())
+            .replace("MM", if(month < 10) "0$month" else month.toString())
             .replace("yyyy", year.toString())
+    }
+
+    companion object {
+        fun fromString(date: String): FrequencyDate {
+            val dateInfo = date.split("/")
+            return FrequencyDate(
+                day = dateInfo[0].toInt(),
+                month = dateInfo[1].toInt(),
+                year = if(dateInfo.size == 3) dateInfo[2].toInt() else 0
+            )
+        }
     }
 }
 
-fun FrequencyDate.fromString(date: String): FrequencyDate {
-    val dateInfo = date.split("/")
-    return FrequencyDate(
-        day = dateInfo[0].toInt(),
-        month = dateInfo[1].toInt(),
-        year = dateInfo[2].toInt()
-    )
-}
