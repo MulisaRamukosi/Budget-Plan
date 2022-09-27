93b1951ff1f4d330c92f031932a42a13dedcb948
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-27 20:09:14 +0200

    added months constants

---
+++ domain/src/main/java/com/puzzle/industries/domain/constants/Months.kt
@@ -0,0 +1,16 @@
+package com.puzzle.industries.domain.constants
+
+enum class Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
