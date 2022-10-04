41fec5e6495fd63d59906d00d85d98435efc3fc6
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-04 22:25:19 +0200

    optimized state flows

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/budget/AddEditIncomeViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/budget/AddEditIncomeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.puzzle.industries.budgetplan.factory.FrequencyDateFactory
 import com.puzzle.industries.budgetplan.factory.viewModel.AddEditIncomeViewModelFactory
+import com.puzzle.industries.budgetplan.util.configs.TextFieldsConfig
 import com.puzzle.industries.domain.constants.FrequencyType
 import com.puzzle.industries.domain.constants.WeekDays
 import com.puzzle.industries.domain.models.income.Income
@@ -34,34 +35,46 @@ class AddEditIncomeViewModel @AssistedInject constructor(
             value = prevIncome?.title ?: ""
         )
     }
-    val title: StateFlow<String> = _title.asStateFlow()
-    val onTitleChange: (String) -> Unit = { it -> _title.value = it }
+    val title: StateFlow<String> by lazy { _title.asStateFlow() }
+    val onTitleChange: (String) -> Unit = { newTitle ->
+        if (newTitle.length <= TextFieldsConfig.singleLineTextMaxChar) {
+            _title.value = newTitle
+            updateRequiredInputState()
+        }
+    }
 
     private val _amount: MutableStateFlow<Double> by lazy {
         MutableStateFlow(
             value = prevIncome?.amount ?: 0.0
         )
     }
-    val amount: StateFlow<Double> = _amount.asStateFlow()
-    val onAmountChange: (Double) -> Unit = { it -> _amount.value = it }
+    val amount: StateFlow<Double> by lazy { _amount.asStateFlow() }
+    val onAmountChange: (Double) -> Unit = { newAmount ->
+        _amount.value = newAmount
+        updateRequiredInputState()
+    }
 
     private val _description: MutableStateFlow<String> by lazy {
         MutableStateFlow(
             value = prevIncome?.description ?: ""
         )
     }
-    val description: StateFlow<String> = _description.asStateFlow()
-    val onDescriptionChange: (String) -> Unit = { it -> _description.value = it }
+    val description: StateFlow<String> by lazy { _description.asStateFlow() }
+    val onDescriptionChange: (String) -> Unit = { newDesc ->
+        if (newDesc.length <= TextFieldsConfig.multiLineTextMaxChar) {
+            _description.value = newDesc
+        }
+    }
 
     private val _frequencyType: MutableStateFlow<FrequencyType> by lazy {
         MutableStateFlow(
             value = prevIncome?.frequencyType ?: FrequencyType.MONTHLY
         )
     }
-    val frequencyType: StateFlow<FrequencyType> = _frequencyType.asStateFlow()
-    val onFrequencyTypeChange: (FrequencyType) -> Unit = { it ->
-        _frequencyType.value = it
-        _frequencyWhen.value = when (it) {
+    val frequencyType: StateFlow<FrequencyType> by lazy { _frequencyType.asStateFlow() }
+    val onFrequencyTypeChange: (FrequencyType) -> Unit = { newFrequencyType ->
+        _frequencyType.value = newFrequencyType
+        _frequencyWhen.value = when (newFrequencyType) {
             FrequencyType.ONCE_OFF -> FrequencyDateFactory.createCurrentDate().toString()
             FrequencyType.MONTHLY -> "1"
             FrequencyType.DAILY -> ""
@@ -75,11 +88,26 @@ class AddEditIncomeViewModel @AssistedInject constructor(
             value = prevIncome?.frequencyWhen ?: "1"
         )
     }
-    val frequencyWhen: StateFlow<String> = _frequencyWhen.asStateFlow()
-    val onFrequencyWhenChange: (String) -> Unit = { it -> _frequencyWhen.value = it }
+    val frequencyWhen: StateFlow<String> by lazy { _frequencyWhen.asStateFlow() }
+    val onFrequencyWhenChange: (String) -> Unit =
+        { newFrequencyWhen -> _frequencyWhen.value = newFrequencyWhen }
 
-    fun allRequiredInputsProvided(): Boolean =
-        _title.value.isBlank().not() && _amount.value > 0
+    private val _crudActionReason: MutableStateFlow<String> by lazy { MutableStateFlow(value = "") }
+    val crudActionReason: String
+        get() = _crudActionReason.value
+    val onCrudActionReasonChange: (String) -> Unit =
+        { newCrudActionReason -> _crudActionReason.value = newCrudActionReason }
+
+    private val _allRequiredInputsProvided: MutableStateFlow<Boolean> by lazy {
+        MutableStateFlow(value = _title.value.isBlank().not() && _amount.value > 0)
+    }
+    val allRequiredInputsProvided: StateFlow<Boolean> = _allRequiredInputsProvided.asStateFlow()
+    private fun updateRequiredInputState() {
+        val newRequiredInputState = _title.value.isBlank().not() && _amount.value > 0
+        if (newRequiredInputState != _allRequiredInputsProvided.value) {
+            _allRequiredInputsProvided.value = newRequiredInputState
+        }
+    }
 
     val isUpdating: Boolean = prevIncome != null
 
