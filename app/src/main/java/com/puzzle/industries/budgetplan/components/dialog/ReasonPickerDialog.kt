326cef87ed588e5f11cda2b33ddedfbc65723ab2
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-04 22:48:01 +0200

    created reason picker dialog component

---
+++ app/src/main/java/com/puzzle/industries/budgetplan/components/dialog/ReasonPickerDialog.kt
@@ -0,0 +1,45 @@
+package com.puzzle.industries.budgetplan.components.dialog
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.puzzle.industries.budgetplan.components.picker.ReasonPicker
+import com.puzzle.industries.budgetplan.components.picker.reasonPickerDefaultActions
+import com.puzzle.industries.budgetplan.theme.spacing
+
+@Composable
+fun ReasonPickerDialog(
+    title: String,
+    reasonSupportingText: String,
+    dismissOnClickOutside: Boolean = false,
+    reasons: List<String>,
+    preselectedReason: String = "",
+    positiveActionText: String,
+    onPositiveActionClick: () -> Unit,
+    onDoNotSpecifyClick: () -> Unit,
+    onReasonChange: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    ViewAlertDialog(
+        onDismiss = onDismiss,
+        titleSection = viewAlertDialogDefaultTitle(
+            title = title,
+            onDismiss = onDismiss
+        ),
+        actions = reasonPickerDefaultActions(
+            doNotSpecifyClick = onDoNotSpecifyClick,
+            positiveActionText = positiveActionText,
+            onPositiveActionClick = onPositiveActionClick
+        ),
+        dismissOnClickOutside = dismissOnClickOutside
+    ) {
+        ReasonPicker(
+            modifier = Modifier.padding(horizontal = MaterialTheme.spacing.medium),
+            supportingText = reasonSupportingText,
+            reason = preselectedReason.ifBlank { reasons[0] },
+            reasons = reasons,
+            onReasonChange = onReasonChange
+        )
+    }
+}
