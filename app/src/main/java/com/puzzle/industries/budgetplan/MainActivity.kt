0b1a81223efc260edb7b5a24c5f2264a9844089c
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-07 15:23:25 +0200

    changed viewModelFactory logic

--- app/src/main/java/com/puzzle/industries/budgetplan/MainActivity.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/MainActivity.kt
@@ -5,16 +5,12 @@ package com.puzzle.industries.budgetplan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.puzzle.industries.budgetplan.factory.viewModel.AddEditIncomeViewModelFactory
+import com.puzzle.industries.budgetplan.factory.viewModel.AddEditIncomeViewModelAssistedFactory
 import com.puzzle.industries.budgetplan.navigation.AppScreensNavHost
 import com.puzzle.industries.budgetplan.theme.BudgetPlanTheme
-import com.puzzle.industries.budgetplan.viewModels.budget.AddEditIncomeViewModel
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,7 +23,7 @@ class MainActivity : ComponentActivity() {
     @EntryPoint
     @InstallIn(ActivityComponent::class)
     interface ViewModelFactoryProvider {
-        fun addEditIncomeViewModelFactory(): AddEditIncomeViewModelFactory
+        fun addEditIncomeViewModelFactory(): AddEditIncomeViewModelAssistedFactory
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
