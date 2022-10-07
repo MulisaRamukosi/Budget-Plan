f9fcafd2b03513537feae0e103c048aa175ef7dd
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-07 15:48:47 +0200

    uppercased variables

--- app/src/main/java/com/puzzle/industries/budgetplan/util/configs/TextFieldsConfig.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/util/configs/TextFieldsConfig.kt
@@ -2,6 +2,6 @@ package com.puzzle.industries.budgetplan.util.configs
 
 object TextFieldsConfig {
 
-    const val singleLineTextMaxChar = 128
-    const val multiLineTextMaxChar = 255
+    const val SINGLE_LINE_TEXT_MAX_CHAR = 128
+    const val MULTI_LINE_TEXT_MAX_CHAR = 255
 }
