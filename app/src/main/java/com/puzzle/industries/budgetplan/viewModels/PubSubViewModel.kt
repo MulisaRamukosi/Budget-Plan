84bde391de05270923bd5ac5ce03847f8c969b58
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-07 15:23:56 +0200

    changed class to abstract class

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/PubSubViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/PubSubViewModel.kt
@@ -1,12 +1,11 @@
-package com.puzzle.industries.budgetplan.viewModels.custom
+package com.puzzle.industries.budgetplan.viewModels
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import javax.inject.Inject
 
-open class PubSubViewModel<T> @Inject constructor(private val initial: T) : ViewModel() {
+abstract class PubSubViewModel<T> constructor(private val initial: T) : ViewModel() {
 
     private val pub: MutableStateFlow<T> by lazy {
         MutableStateFlow(value = initial)
