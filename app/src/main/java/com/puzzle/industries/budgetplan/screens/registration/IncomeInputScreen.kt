286aeda5bf0762820f2d82210b4600e12a72882a
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-11 06:36:04 +0200

    removed unused imports

--- app/src/main/java/com/puzzle/industries/budgetplan/screens/registration/IncomeInputScreen.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/screens/registration/IncomeInputScreen.kt
@@ -4,12 +4,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
