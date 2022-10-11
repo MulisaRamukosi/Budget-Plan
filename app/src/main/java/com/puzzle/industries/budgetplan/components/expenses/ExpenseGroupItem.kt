312803ab26a1b6b6fe6ca8fc00ffb57af2e4b6f6
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-11 06:48:53 +0200

    finished off implementation and optimization of component

--- app/src/main/java/com/puzzle/industries/budgetplan/components/expenses/ExpenseGroupItem.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/components/expenses/ExpenseGroupItem.kt
@@ -1,76 +1,159 @@
 @file:OptIn(
-    ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3WindowSizeClassApi::class,
-    ExperimentalMaterial3WindowSizeClassApi::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class
+    ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class
 )
 
 package com.puzzle.industries.budgetplan.components.expenses
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Payments
 import androidx.compose.material3.*
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.puzzle.industries.budgetplan.R
 import com.puzzle.industries.budgetplan.components.layout.MiniCaption
 import com.puzzle.industries.budgetplan.components.layout.ModifiableItemWrapper
 import com.puzzle.industries.budgetplan.components.layout.TitleAndDescription
 import com.puzzle.industries.budgetplan.components.spacer.H_S_Space
 import com.puzzle.industries.budgetplan.components.spacer.V_M_Space
+import com.puzzle.industries.budgetplan.components.spacer.V_S_Space
+import com.puzzle.industries.budgetplan.components.symbols.BulletPoint
 import com.puzzle.industries.budgetplan.theme.BudgetPlanTheme
+import com.puzzle.industries.budgetplan.theme.colorPickerColors
 import com.puzzle.industries.budgetplan.theme.spacing
-
-
-@Composable
-fun ExpenseGroupItem() {
-    ExpenseGroup(
-        title = "Expense Title",
-        description = "some description",
-        amount = 5000.0,
-        numOfExpenses = 12
-    )
-}
+import com.puzzle.industries.domain.constants.FrequencyType
+import com.puzzle.industries.domain.models.expense.Expense
+import com.puzzle.industries.domain.models.expenseGroup.ExpenseGroup
+import com.puzzle.industries.domain.models.expenseGroup.ExpenseGroupWithExpenses
+import java.util.*
 
 @Composable
-private fun ExpenseGroup(
-    title: String,
-    description: String,
-    amount: Double,
-    numOfExpenses: Int
+fun ExpenseGroupItem(
+    currencySymbol: String,
+    expenseGroupWithExpenses: ExpenseGroupWithExpenses,
+    onAddExpenseClick: () -> Unit,
+    onEditExpenseGroupClick: (ExpenseGroup) -> Unit,
+    onDeleteExpenseGroupClick: (ExpenseGroupWithExpenses) -> Unit,
+    onEditExpenseClick: (Expense) -> Unit,
+    onDeleteExpenseClick: (Expense) -> Unit
 ) {
-    ModifiableItemWrapper(modifier = Modifier.fillMaxWidth(), {}, {}) {
-        Column {
-            Column(modifier = it) {
-                TitleAndDescription(title = title, description = description)
+    var showExpenses by rememberSaveable { mutableStateOf(false) }
+    val expenseGroup = expenseGroupWithExpenses.expenseGroup
+    val expenses = expenseGroupWithExpenses.expenses
 
-                V_M_Space()
+    val totalExpenseAmount = expenses.sumOf { expense -> expense.amount }
 
-                OutcomeAmount(amount = amount)
+    Column {
+        ModifiableItemWrapper(
+            modifier = Modifier.fillMaxWidth(),
+            onEditClick = { onEditExpenseGroupClick(expenseGroup) },
+            onDeleteClick = { onDeleteExpenseGroupClick(expenseGroupWithExpenses) }
+        ) {
+            Column {
+                Column(modifier = it) {
+                    BulletPoint(
+                        color = MaterialTheme.colorPickerColors.getColor(expenseGroup.colorId),
+                        size = 8.dp
+                    )
+
+                    TitleAndDescription(
+                        title = expenseGroup.name,
+                        description = expenseGroup.description
+                    )
+
+                    V_M_Space()
+
+                    OutcomeAmount(currencySymbol = currencySymbol, amount = totalExpenseAmount)
+                }
+
+                ExpenseOptions(
+                    modifier = Modifier.padding(start = MaterialTheme.spacing.medium),
+                    showExpenses = showExpenses,
+                    enableShowExpensesOption = expenses.isEmpty().not(),
+                    onViewExpenseClicked = {
+                        showExpenses = !showExpenses
+                    },
+                    onAddExpense = onAddExpenseClick
+                )
+
+                MiniCaption(
+                    modifier = it,
+                    imageVector = Icons.Rounded.Payments,
+                    message = stringResource(id = R.string.num_of_expenses, expenses.size)
+                )
             }
-
-            ExpenseOptions(modifier = Modifier.padding(start = MaterialTheme.spacing.medium))
-
-            MiniCaption(
-                modifier = it,
-                imageVector = Icons.Rounded.Payments,
-                message = stringResource(id = R.string.num_of_expenses, numOfExpenses)
-            )
         }
 
+        ExpenseItems(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = MaterialTheme.spacing.medium),
+            showExpenses = showExpenses,
+            expenses = expenses,
+            currencySymbol = currencySymbol,
+            onEditExpenseClick = onEditExpenseClick,
+            onDeleteExpenseClick = onDeleteExpenseClick
+        )
     }
 }
 
 @Composable
-private fun ExpenseOptions(modifier: Modifier){
+private fun ExpenseItems(
+    modifier: Modifier,
+    showExpenses: Boolean,
+    expenses: List<Expense>,
+    currencySymbol: String,
+    onEditExpenseClick: (Expense) -> Unit,
+    onDeleteExpenseClick: (Expense) -> Unit
+) {
+    AnimatedVisibility(
+        visible = showExpenses,
+        enter = slideInVertically()
+                + expandVertically(expandFrom = Alignment.Top)
+                + scaleIn(transformOrigin = TransformOrigin(0.5f, 0f))
+                + fadeIn(initialAlpha = 0.3f),
+        exit = slideOutVertically(targetOffsetY = {40})
+                + shrinkVertically(shrinkTowards = Alignment.Top)
+                + fadeOut()
+                + scaleOut(targetScale = 1.2f)
+    ) {
+        Column(
+            modifier = modifier
+        ) {
+            expenses.forEach { expense ->
+                V_S_Space()
+                ExpenseItem(
+                    modifier = Modifier.fillMaxWidth(),
+                    currencySymbol = currencySymbol,
+                    expense = expense,
+                    onEditClick = onEditExpenseClick,
+                    onDeleteClick = onDeleteExpenseClick
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun ExpenseOptions(
+    modifier: Modifier,
+    showExpenses: Boolean,
+    enableShowExpensesOption: Boolean,
+    onViewExpenseClicked: () -> Unit,
+    onAddExpense: () -> Unit
+) {
     val chipRowScrollState = rememberScrollState()
 
     Row(
@@ -79,8 +162,16 @@ private fun ExpenseOptions(modifier: Modifier){
 
     ) {
         AssistChip(
-            label = { Text(text = stringResource(id = R.string.view_expenses)) },
-            onClick = {}
+            label = {
+                Text(
+                    text = stringResource(
+                        id = if (showExpenses) R.string.view_less_expenses
+                        else R.string.view_more_expenses
+                    )
+                )
+            },
+            onClick = onViewExpenseClicked,
+            enabled = enableShowExpensesOption
         )
 
         H_S_Space()
@@ -93,7 +184,7 @@ private fun ExpenseOptions(modifier: Modifier){
                 )
             },
             label = { Text(text = stringResource(id = R.string.add_expense)) },
-            onClick = {}
+            onClick = onAddExpense
         )
     }
 }
@@ -102,7 +193,35 @@ private fun ExpenseOptions(modifier: Modifier){
 @Composable
 fun PreviewExpenseGroupItem() {
     BudgetPlanTheme(dynamicColor = false) {
-        ExpenseGroupItem()
+        ExpenseGroupItem(
+            currencySymbol = "R",
+            expenseGroupWithExpenses = ExpenseGroupWithExpenses(
+                expenseGroup = ExpenseGroup(name = "dummy title", description = "some desc", colorId = ""),
+                expenses = listOf(
+                    Expense(
+                        expenseGroupId = UUID.randomUUID(),
+                        name = "exp1",
+                        amount = 12.0,
+                        description = "some desc",
+                        frequencyType = FrequencyType.MONTHLY,
+                        frequencyWhen = "1"
+                    ),
+                    Expense(
+                        expenseGroupId = UUID.randomUUID(),
+                        name = "exp2",
+                        amount = 14.0,
+                        description = "some desc",
+                        frequencyType = FrequencyType.MONTHLY,
+                        frequencyWhen = "1"
+                    )
+                )
+            ),
+            onEditExpenseClick = {},
+            onEditExpenseGroupClick = {},
+            onDeleteExpenseClick = {},
+            onDeleteExpenseGroupClick = {},
+            onAddExpenseClick = {}
+        )
     }
 
 }
--- app/src/main/java/com/puzzle/industries/budgetplan/components/expenses/ExpenseItem.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/components/expenses/ExpenseItem.kt
@@ -1,8 +1,10 @@
 package com.puzzle.industries.budgetplan.components.expenses
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Alarm
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,27 +13,47 @@ import com.puzzle.industries.budgetplan.components.layout.MiniCaption
 import com.puzzle.industries.budgetplan.components.layout.ModifiableItemWrapper
 import com.puzzle.industries.budgetplan.components.layout.TitleAndDescription
 import com.puzzle.industries.budgetplan.components.spacer.V_M_Space
+import com.puzzle.industries.budgetplan.components.spacer.V_S_Space
 import com.puzzle.industries.budgetplan.theme.BudgetPlanTheme
+import com.puzzle.industries.budgetplan.theme.colorPickerColors
+import com.puzzle.industries.budgetplan.util.buildFrequencyMessage
+import com.puzzle.industries.domain.constants.FrequencyType
+import com.puzzle.industries.domain.models.expense.Expense
+import java.util.*
 
 @Composable
-private fun ExpenseItem(
+fun ExpenseItem(
     modifier: Modifier,
-    title: String,
-    amount: Double,
-    frequency: String
+    currencySymbol: String,
+    expense: Expense,
+    onEditClick: (Expense) -> Unit,
+    onDeleteClick: (Expense) -> Unit
 ) {
-    ModifiableItemWrapper(modifier = modifier, {}, {}) {
+    ModifiableItemWrapper(
+        modifier = modifier,
+        onEditClick = { onEditClick(expense) },
+        onDeleteClick = { onDeleteClick(expense) }
+    ) {
         Column(modifier = it) {
 
-            TitleAndDescription(title = title)
+            TitleAndDescription(title = expense.name, description = expense.description)
+
+            V_S_Space()
+
+            OutcomeAmount(
+                currencySymbol = currencySymbol,
+                amountTextStyle = MaterialTheme.typography.bodyLarge,
+                amount = expense.amount
+            )
 
             V_M_Space()
 
-            OutcomeAmount(amount = amount)
-
-            V_M_Space()
-
-            MiniCaption(imageVector = Icons.Rounded.Alarm, message = frequency)
+            MiniCaption(
+                imageVector = Icons.Rounded.Alarm, message = buildFrequencyMessage(
+                    type = expense.frequencyType,
+                    message = expense.frequencyWhen
+                )
+            )
         }
     }
 }
@@ -39,8 +61,21 @@ private fun ExpenseItem(
 @Composable
 @Preview
 @ExperimentalMaterial3WindowSizeClassApi
-fun ExpenseItemPreview(){
+fun ExpenseItemPreview() {
     BudgetPlanTheme(dynamicColor = false) {
-        ExpenseItem(modifier = Modifier.fillMaxWidth(), title = "Expense Item", amount = 13.0, frequency = "Monthly")
+        ExpenseItem(
+            modifier = Modifier.fillMaxWidth(),
+            currencySymbol = "R",
+            expense = Expense(
+                expenseGroupId = UUID.randomUUID(),
+                name = "some expense",
+                amount = 124.0,
+                description = "some desc",
+                frequencyType = FrequencyType.MONTHLY,
+                frequencyWhen = "1"
+            ),
+            onEditClick = {},
+            onDeleteClick = {}
+        )
     }
 }
