332d515a29e6572ae51ccdfc5c795c85ee0ac0f1
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-10-25 07:01:08 +0200

    stabilized code

--- domain/src/main/java/com/puzzle/industries/domain/models/FrequencyDate.kt
+++ domain/src/main/java/com/puzzle/industries/domain/models/FrequencyDate.kt
@@ -1,35 +1,52 @@
 package com.puzzle.industries.domain.models
 
-import com.puzzle.industries.domain.constants.Constants
+import com.puzzle.industries.domain.constants.DateConfigs
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class FrequencyDate(
     val day: Int,
     val month: Int,
     val year: Int,
 ) {
-    fun toDayMonthString(): String{
-        return Constants.DATE_FORMAT
-            .replace("dd", if(day < 10) "0$day" else day.toString())
-            .replace("MM", if(month < 10) "0$month" else month.toString())
-            .replace("/yyyy", "")
+    fun formatToDayMonth(): String {
+        val date = Calendar.getInstance().apply {
+            set(year, month, day)
+        }
+
+        return SimpleDateFormat(DateConfigs.DAY_MONTH_FORMAT, Locale.US).format(date.time)
     }
 
     override fun toString(): String {
-        return Constants.DATE_FORMAT
-            .replace("dd", if(day < 10) "0$day" else day.toString())
-            .replace("MM", if(month < 10) "0$month" else month.toString())
-            .replace("yyyy", year.toString())
+        val date = Calendar.getInstance().apply {
+            set(year, month, day)
+        }
+        return SimpleDateFormat(DateConfigs.DATE_FORMAT, Locale.US).format(date.time)
     }
 
     companion object {
-        fun parse(date: String): FrequencyDate {
-            val dateInfo = date.split("/")
+        fun parseDayMonth(date: String): FrequencyDate {
+            val parsedDate = SimpleDateFormat(DateConfigs.DAY_MONTH_FORMAT, Locale.US).parse(date)
+            val calendar = Calendar.getInstance()
+            calendar.time = parsedDate
+
             return FrequencyDate(
-                day = dateInfo[0].toInt(),
-                month = dateInfo[1].toInt(),
-                year = if(dateInfo.size == 3) dateInfo[2].toInt() else 0
+                day = calendar.get(Calendar.DAY_OF_MONTH),
+                month = calendar.get(Calendar.MONTH),
+                year = 0
+            )
+        }
+
+        fun parseDate(date: String): FrequencyDate {
+            val parsedDate = SimpleDateFormat(DateConfigs.DATE_FORMAT, Locale.US).parse(date)
+            val calendar = Calendar.getInstance()
+            calendar.time = parsedDate
+
+            return FrequencyDate(
+                day = calendar.get(Calendar.DAY_OF_MONTH),
+                month = calendar.get(Calendar.MONTH),
+                year = calendar.get(Calendar.YEAR)
             )
         }
     }
 }
-
