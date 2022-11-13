b9bcb8b963ca2cf15fb39afcb17ac415411375fb
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-13 15:44:14 +0200

    moved out datastore DI into a different module and added countryCurrencyService DI

--- data/src/main/java/com/puzzle/industries/data/dependencyInjection/ServiceModule.kt
+++ data/src/main/java/com/puzzle/industries/data/dependencyInjection/ServiceModule.kt
@@ -1,12 +1,10 @@
 package com.puzzle.industries.data.dependencyInjection
 
 import android.content.Context
+import com.puzzle.industries.data.services.CountryCurrencyServiceImpl
 import com.puzzle.industries.data.services.ReminderServiceImpl
 import com.puzzle.industries.data.storage.datastore.*
-import com.puzzle.industries.domain.datastores.BPlanGenDayDataStore
-import com.puzzle.industries.domain.datastores.CountryCurrencyDataStore
-import com.puzzle.industries.domain.datastores.DebtDataStore
-import com.puzzle.industries.domain.datastores.LaunchTrackingDataStore
+import com.puzzle.industries.domain.datastores.*
 import com.puzzle.industries.domain.services.*
 import dagger.Module
 import dagger.Provides
@@ -21,26 +19,10 @@ internal class ServiceModule {
 
     @Singleton
     @Provides
-    fun provideLaunchTrackingService(@ApplicationContext context: Context): LaunchTrackingDataStore =
-        LaunchTrackingDataStoreImpl(context = context)
-
-    @Singleton
-    @Provides
-    fun provideCountryCurrencyPrefService(@ApplicationContext context: Context): CountryCurrencyDataStore =
-        CountryCurrencyDataStoreImpl(context = context)
-
-    @Singleton
-    @Provides
-    fun provideDebtPrefService(@ApplicationContext context: Context): DebtDataStore =
-        DebtDataStoreImpl(context = context)
-
-    @Singleton
-    @Provides
-    fun provideBPlanGenDayPrefService(@ApplicationContext context: Context): BPlanGenDayDataStore =
-        BPlanGenDayDataStoreImpl(context = context)
-
-    @Singleton
-    @Provides
     fun provideReminderService(@ApplicationContext context: Context): ReminderService =
         ReminderServiceImpl(context = context)
+
+    @Singleton
+    @Provides
+    fun provideCountryCurrencyService(): CountryCurrencyService = CountryCurrencyServiceImpl()
 }
