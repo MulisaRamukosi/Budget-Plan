517f9e9ffbde6e27e0f28d18f24c8bf147d7d27a
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-13 15:41:50 +0200

    added currencyName functionality in store

--- data/src/main/java/com/puzzle/industries/data/storage/datastore/CountryCurrencyDataStoreImpl.kt
+++ data/src/main/java/com/puzzle/industries/data/storage/datastore/CountryCurrencyDataStoreImpl.kt
@@ -6,25 +6,35 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.puzzle.industries.data.util.config.DataStoreNameConfig
+import com.puzzle.industries.domain.constants.CountryCurrencyConfig
 import com.puzzle.industries.domain.datastores.CountryCurrencyDataStore
+import com.puzzle.industries.domain.models.CountryCurrency
+import com.puzzle.industries.domain.services.CountryCurrencyService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-private val Context.currencyStore: DataStore<Preferences> by preferencesDataStore(name = "currency")
+private val Context.currencyStore: DataStore<Preferences> by preferencesDataStore(name = DataStoreNameConfig.COUNTRY_CURRENCY)
 
 internal class CountryCurrencyDataStoreImpl(private val context: Context) :
     CountryCurrencyDataStore {
 
-    private val currencySymbolKey = stringPreferencesKey(name = "csk")
+    private val currencyNameKey = stringPreferencesKey(name = "selectedCurrencyNameKey")
+    private val currencySymbolKey = stringPreferencesKey(name = "selectedCurrencySymbolKey")
 
-    override suspend fun saveCurrencySymbol(symbol: String) {
+    override suspend fun saveCountryCurrency(countryCurrency: CountryCurrency) {
         context.currencyStore.edit { settings ->
-            settings[currencySymbolKey] = symbol
+            settings[currencyNameKey] = countryCurrency.currency
+            settings[currencySymbolKey] = countryCurrency.symbol
         }
     }
 
-    override fun getCurrencySymbol(): Flow<String> = context.currencyStore.data.map { preferences ->
-        preferences[currencySymbolKey] ?: "R"
+
+    override fun getSelectedCurrencySymbol(): Flow<String> = context.currencyStore.data.map { preferences ->
+        preferences[currencySymbolKey] ?: CountryCurrencyConfig.DEFAULT_SYMBOL
     }
 
+    override fun getSelectedCurrencyName(): Flow<String> = context.currencyStore.data.map { preferences ->
+        preferences[currencyNameKey] ?: CountryCurrencyConfig.DEFAULT_CURRENCY
+    }
 }
