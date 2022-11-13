4f278af63a0a168c57b2f9897dfff6bd2cac8390
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-13 15:57:45 +0200

    added more functionality to view model

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/registrationFlow/CurrencyViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/registrationFlow/CurrencyViewModel.kt
@@ -1,31 +1,18 @@
 package com.puzzle.industries.budgetplan.viewModels.registrationFlow
 
-import com.puzzle.industries.budgetplan.R
-import com.puzzle.industries.budgetplan.data.CountryCurrency
 import com.puzzle.industries.budgetplan.viewModels.PubSubViewModel
+import com.puzzle.industries.domain.models.CountryCurrency
+import com.puzzle.industries.domain.services.CountryCurrencyService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CurrencyViewModel @Inject constructor() : PubSubViewModel<Int>(initial = 0) {
-    val countries: List<CountryCurrency> = listOf(
-        CountryCurrency(
-            currency = "ZAR",
-            country = "South Africa",
-            symbol = "R",
-            flagId = R.drawable.ic_flag_south_africa
-        ),
-        CountryCurrency(
-            currency = "RWF",
-            country = "Rwanda",
-            symbol = "R₣",
-            flagId = R.drawable.ic_flag_rwanda
-        ),
-        CountryCurrency(
-            currency = "LSL",
-            country = "Lesotho",
-            symbol = "L",
-            flagId = R.drawable.ic_flag_lesotho
-        )
-    )
+class CurrencyViewModel @Inject constructor(private val countryCurrencyService: CountryCurrencyService) :
+    PubSubViewModel<CountryCurrency>(initial = countryCurrencyService.getDefaultCountryCurrency()) {
+
+    val allCountries: List<CountryCurrency>
+        get() = countryCurrencyService.getAllCountries()
+
+    fun getCountryCurrencyByCurrencyName(currencyName: String): CountryCurrency =
+        countryCurrencyService.getCountryCurrencyByCurrencyName(currency = currencyName)
 }
