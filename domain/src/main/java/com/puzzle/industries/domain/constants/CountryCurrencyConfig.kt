ba82c583b8b0825b257ce49fd9eee510d073e05b
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-13 14:51:16 +0200

    added constant values used in services

---
+++ domain/src/main/java/com/puzzle/industries/domain/constants/CountryCurrencyConfig.kt
@@ -0,0 +1,6 @@
+package com.puzzle.industries.domain.constants
+
+object CountryCurrencyConfig {
+    const val DEFAULT_CURRENCY = "ZAR"
+    const val DEFAULT_SYMBOL = "R"
+}
---
+++ domain/src/main/java/com/puzzle/industries/domain/constants/ThemeType.kt
@@ -0,0 +1,7 @@
+package com.puzzle.industries.domain.constants
+
+enum class ThemeType {
+    SYSTEM_DEPENDENT,
+    LIGHT,
+    DARK
+}
