76450e883c239d655345cdaf555fd9e4e72b7fb6
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-13 15:40:28 +0200

    moved datastore names into a config file

---
+++ data/src/main/java/com/puzzle/industries/data/util/config/DataStoreNameConfig.kt
@@ -0,0 +1,10 @@
+package com.puzzle.industries.data.util.config
+
+object DataStoreNameConfig {
+    const val B_PLAN_GEN_DAY = "BPlanGenDay"
+    const val COUNTRY_CURRENCY = "Currency"
+    const val DEBT = "Debt"
+    const val LAUNCH_TRACKING = "Launch"
+    const val REMINDER = "Reminder"
+    const val THEME = "Theme"
+}
