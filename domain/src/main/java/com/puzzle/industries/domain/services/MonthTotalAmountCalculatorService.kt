2cc4ddf4b9b71d825fd525c3b71e5b25a643e44e
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-17 22:51:25 +0200

    added functions to calculate totals for current month

--- domain/src/main/java/com/puzzle/industries/domain/services/MonthTotalAmountCalculatorService.kt
+++ domain/src/main/java/com/puzzle/industries/domain/services/MonthTotalAmountCalculatorService.kt
@@ -5,6 +5,8 @@ import com.puzzle.industries.domain.models.expense.Expense
 import com.puzzle.industries.domain.models.income.Income
 
 interface MonthTotalAmountCalculatorService {
+    fun calculateTotalIncomesForCurrentMonth(incomes: List<Income>): Double
+    fun calculateTotalExpensesForCurrentMonth(expenses: List<Expense>): Double
     fun calculateTotalIncomesForMonth(month: Months, incomes: List<Income>): Double
     fun calculateTotalExpensesForMonth(month: Months, expenses: List<Expense>): Double
 }
