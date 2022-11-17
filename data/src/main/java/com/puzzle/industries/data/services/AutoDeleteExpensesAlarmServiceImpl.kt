28d16df5a151a324401cfd890ae6e6c1c972c63c
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-17 22:55:16 +0200

    implemented service

---
+++ data/src/main/java/com/puzzle/industries/data/services/AutoDeleteExpensesAlarmServiceImpl.kt
@@ -0,0 +1,63 @@
+package com.puzzle.industries.data.services
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.puzzle.industries.data.broadcastReceivers.AutoDeleteExpensesBroadcastReceiver
+import com.puzzle.industries.data.services.alarmManager.AlarmManagerService
+import com.puzzle.industries.domain.datastores.AutoDeleteExpenseDataStore
+import com.puzzle.industries.domain.services.AutoDeleteExpensesAlarmService
+import com.puzzle.industries.domain.services.CalendarService
+import java.util.*
+
+internal class AutoDeleteExpensesAlarmServiceImpl constructor(
+    private val context: Context,
+    private val calendarService: CalendarService,
+    private val alarmManagerService: AlarmManagerService,
+    private val autoDeleteExpenseDataStore: AutoDeleteExpenseDataStore
+) : AutoDeleteExpensesAlarmService {
+
+    private val requestCode = 8028
+
+    private val autoDeleteExpenseWithReminderPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+        context,
+        requestCode,
+        Intent(context, AutoDeleteExpensesBroadcastReceiver::class.java),
+        PendingIntent.FLAG_IMMUTABLE
+    )
+
+    override suspend fun setAutoDeleteExpenseAlarm() {
+        autoDeleteExpenseDataStore.getAutoDeleteExpenseState().collect { enabled ->
+            if(enabled && !alarmIsSet()){
+                setAlarm()
+            }
+            else{
+                alarmManagerService.cancelAlarm(pendingIntent = autoDeleteExpenseWithReminderPendingIntent)
+            }
+        }
+    }
+
+    private fun setAlarm() {
+        val calendar = calendarService.getInstance()
+
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 5)
+
+        alarmManagerService.setRepetitiveAlarm(
+            triggerAtMillis = calendar.timeInMillis,
+            intervalMillis = AlarmManager.INTERVAL_DAY,
+            pendingIntent = autoDeleteExpenseWithReminderPendingIntent
+        )
+    }
+
+    private fun alarmIsSet(): Boolean {
+        return PendingIntent.getBroadcast(
+            context,
+            requestCode,
+            Intent(context, AutoDeleteExpensesBroadcastReceiver::class.java),
+            PendingIntent.FLAG_NO_CREATE
+        ) != null
+    }
+}
