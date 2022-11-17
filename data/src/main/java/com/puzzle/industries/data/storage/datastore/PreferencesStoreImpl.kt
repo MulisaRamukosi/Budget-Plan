0d0c737e3616e891fb316e92b518452d493967ef
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-17 22:53:06 +0200

    side effect of rename

--- data/src/main/java/com/puzzle/industries/data/storage/datastore/PreferencesStoreImpl.kt
+++ data/src/main/java/com/puzzle/industries/data/storage/datastore/PreferencesStoreImpl.kt
@@ -3,7 +3,7 @@ package com.puzzle.industries.data.storage.datastore
 import com.puzzle.industries.domain.datastores.*
 
 class PreferencesStoreImpl(
-    override val autoDeleteReminderPref: AutoDeleteReminderDataStore,
+    override val autoDeleteReminderPref: AutoDeleteExpenseDataStore,
     override val bPlanGenDayPref: BPlanGenDayDataStore,
     override val currencyPref: CountryCurrencyDataStore,
     override val debtPref: DebtDataStore,
