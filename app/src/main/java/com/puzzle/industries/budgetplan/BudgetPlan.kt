c52416d3b16f47312b3b5be78fa7e70459d2c373
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-17 23:01:58 +0200

    added registration of alarm service on startup

--- app/src/main/java/com/puzzle/industries/budgetplan/BudgetPlan.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/BudgetPlan.kt
@@ -2,16 +2,22 @@ package com.puzzle.industries.budgetplan
 
 import android.app.Application
 import com.puzzle.industries.data.notification.NotificationChannelService
+import com.puzzle.industries.domain.services.AutoDeleteExpensesAlarmService
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltAndroidApp
 class BudgetPlan : Application() {
 
     @Inject lateinit var notificationChannelService: NotificationChannelService
+    @Inject lateinit var autoDeleteExpensesAlarmService: AutoDeleteExpensesAlarmService
 
     override fun onCreate() {
         super.onCreate()
         notificationChannelService.createNotificationChannels()
+        CoroutineScope(Dispatchers.IO).launch {
+            autoDeleteExpensesAlarmService.setAutoDeleteExpenseAlarm()
+        }
     }
 }
