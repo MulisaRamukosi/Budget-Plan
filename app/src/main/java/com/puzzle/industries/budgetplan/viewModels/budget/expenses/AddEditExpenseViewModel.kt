68e76dbf560630d8492c1134f88327fcbf1a13a0
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-20 22:24:46 +0200

    implemented debt lock + remaining amount

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/budget/expenses/AddEditExpenseViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/budget/expenses/AddEditExpenseViewModel.kt
@@ -4,14 +4,22 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.puzzle.industries.budgetplan.delegates.AddEditItemStateHandlerDelegate
 import com.puzzle.industries.budgetplan.delegates.CoroutineHandlerDelegate
+import com.puzzle.industries.budgetplan.delegates.CurrencySymbolObserverDelegate
 import com.puzzle.industries.budgetplan.delegates.SavedStateHandlerDelegate
 import com.puzzle.industries.budgetplan.delegates.implementation.AddEditItemStateHandlerDelegateImpl
 import com.puzzle.industries.budgetplan.delegates.implementation.CoroutineHandlerDelegateImpl
+import com.puzzle.industries.budgetplan.delegates.implementation.CurrencySymbolObserverDelegateImpl
 import com.puzzle.industries.budgetplan.delegates.implementation.SavedStateHandlerDelegateImpl
 import com.puzzle.industries.budgetplan.util.configs.FrequencyConfig
+import com.puzzle.industries.domain.constants.Months
+import com.puzzle.industries.domain.datastores.CountryCurrencyDataStore
+import com.puzzle.industries.domain.models.DebtCheckResult
 import com.puzzle.industries.domain.models.expense.Expense
+import com.puzzle.industries.domain.services.DebtService
+import com.puzzle.industries.domain.services.MonthTotalAmountCalculatorService
 import dagger.assisted.AssistedInject
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import java.util.*
@@ -19,8 +27,12 @@ import java.util.*
 class AddEditExpenseViewModel @AssistedInject constructor(
     private val savedStateHandle: SavedStateHandle,
     private val expenseGroupId: UUID,
+    private val debtService: DebtService,
+    private val monthTotalAmountCalculatorService: MonthTotalAmountCalculatorService,
+    private val countryCurrencyDataStore: CountryCurrencyDataStore,
     private val prevExpense: Expense?
 ) : ViewModel(),
+    CurrencySymbolObserverDelegate by CurrencySymbolObserverDelegateImpl(countryCurrencyDataStore),
     SavedStateHandlerDelegate by SavedStateHandlerDelegateImpl(savedStateHandle),
     AddEditItemStateHandlerDelegate by AddEditItemStateHandlerDelegateImpl(savedStateHandle),
     CoroutineHandlerDelegate by CoroutineHandlerDelegateImpl() {
@@ -75,6 +87,22 @@ class AddEditExpenseViewModel @AssistedInject constructor(
         getActionReasonStateHandler()
     }
 
+    private val _allowDebt: MutableStateFlow<Boolean> = MutableStateFlow(value = false)
+    val allowDebt: StateFlow<Boolean> = _allowDebt
+
+    private val _debtCheckResult: MutableStateFlow<DebtCheckResult> =
+        MutableStateFlow(
+            value = DebtCheckResult(
+                willBeInDebt = false,
+                debtAmount = 0.0,
+                forMonth = Months.JANUARY,
+                forYear = 2022
+            )
+        )
+    val debtCheckResult: StateFlow<DebtCheckResult> = _debtCheckResult
+
+    val currencySymbol: StateFlow<String> = currencySymbolFlow
+
     val expense: Expense
         get() = Expense(
             id = getStateFlow(key = Expense::id.name, initialValue = UUID.randomUUID()).value,
@@ -92,6 +120,12 @@ class AddEditExpenseViewModel @AssistedInject constructor(
     init {
         cacheExpense()
         initAllInputsValidFlow()
+        initDebtAllowedFlow()
+        initDebtFlows()
+    }
+
+    fun getPayableExpenseAmountBasedOnFrequency(): Double {
+        return monthTotalAmountCalculatorService.calculatePayableExpenseInAMonth(expense)
     }
 
     private fun cacheExpense() {
@@ -107,7 +141,7 @@ class AddEditExpenseViewModel @AssistedInject constructor(
     }
 
     private fun initAllInputsValidFlow() = runCoroutine {
-        val allConditions = combine(
+        val requiredInputsCondition = combine(
             titleStateFlowHandler.valueStateFlow,
             amountStateFlowHandler.valueStateFlow,
             descriptionStateFlowHandler.valueStateFlow,
@@ -116,7 +150,7 @@ class AddEditExpenseViewModel @AssistedInject constructor(
         ) { title, amount, desc, type, _when ->
             var valueChangedCondition = true
 
-            if(isUpdatingConditionHandler.getValue()){
+            if (isUpdatingConditionHandler.getValue()) {
                 valueChangedCondition = title != prevExpense?.name
                         || amount != prevExpense.amount
                         || desc != prevExpense.description
@@ -127,8 +161,39 @@ class AddEditExpenseViewModel @AssistedInject constructor(
             valueChangedCondition && title.isBlank().not() && amount > 0
         }
 
-        allConditions.distinctUntilChanged().collect { allInputsMeetCondition ->
+        val requiredConditions = combine(
+            requiredInputsCondition,
+            allowDebt,
+            debtCheckResult
+        ) { inputsCondition, allowDebt, debtCheckResult ->
+            if (!allowDebt) return@combine inputsCondition && !debtCheckResult.willBeInDebt
+            return@combine inputsCondition
+        }
+
+        requiredConditions.distinctUntilChanged().collect { allInputsMeetCondition ->
             requiredInputsStateFlowHandler.onValueChange(allInputsMeetCondition)
         }
+    }
+
+    private fun initDebtAllowedFlow() = runCoroutine {
+        debtService.getDebtAllowedState().collect { allowDebt ->
+            _allowDebt.value = allowDebt
+        }
+    }
+
+    private fun initDebtFlows() = runCoroutine {
+        combine(
+            amountStateFlowHandler.valueStateFlow,
+            frequencyWhenStateFlowHandler.valueStateFlow
+        ) { _, _ -> expense }
+            .distinctUntilChanged().collect {
+                if (isUpdatingConditionHandler.getValue()) {
+                    _debtCheckResult.value =
+                        debtService.willBeInDebtAfterModifyingExpense(expense = it)
+                } else {
+                    _debtCheckResult.value =
+                        debtService.willBeInDebtAfterAddingExpense(expense = it)
+                }
+            }
     }
 }
