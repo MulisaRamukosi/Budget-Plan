c2bc082b5c771c789af81f0b61ae67fbdad38fce
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-20 22:09:40 +0200

    auto formatting

--- domain/src/main/java/com/puzzle/industries/domain/services/MonthTotalAmountCalculatorService.kt
+++ domain/src/main/java/com/puzzle/industries/domain/services/MonthTotalAmountCalculatorService.kt
@@ -5,6 +5,17 @@ import com.puzzle.industries.domain.models.expense.Expense
 import com.puzzle.industries.domain.models.income.Income
 
 interface MonthTotalAmountCalculatorService {
-    fun calculateTotalIncomesForMonth(month: Months? = null, incomes: List<Income>): Double
-    fun calculateTotalExpensesForMonth(month: Months? = null, expenses: List<Expense>): Double
+    fun calculateTotalIncomesForMonth(
+        month: Months? = null,
+        year: Int? = null,
+        incomes: List<Income>
+    ): Double
+
+    fun calculateTotalExpensesForMonth(
+        month: Months? = null,
+        year: Int? = null,
+        expenses: List<Expense>
+    ): Double
+
+    fun calculatePayableExpenseInAMonth(expense: Expense): Double
 }
