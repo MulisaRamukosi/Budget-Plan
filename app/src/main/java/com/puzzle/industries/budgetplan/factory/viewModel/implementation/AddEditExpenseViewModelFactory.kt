1e914406464b757590fe4fd57f7c9fb54de1d9db
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-20 22:21:47 +0200

    injected new services

--- app/src/main/java/com/puzzle/industries/budgetplan/factory/viewModel/implementation/AddEditExpenseViewModelFactory.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/factory/viewModel/implementation/AddEditExpenseViewModelFactory.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.savedstate.SavedStateRegistryOwner
 import com.puzzle.industries.budgetplan.viewModels.budget.expenses.AddEditExpenseViewModel
+import com.puzzle.industries.domain.datastores.CountryCurrencyDataStore
 import com.puzzle.industries.domain.models.expense.Expense
+import com.puzzle.industries.domain.services.DebtService
+import com.puzzle.industries.domain.services.MonthTotalAmountCalculatorService
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import java.util.*
@@ -13,6 +16,9 @@ import java.util.*
 @Suppress("UNCHECKED_CAST")
 class AddEditExpenseViewModelFactory @AssistedInject constructor(
     @Assisted owner: SavedStateRegistryOwner,
+    private val debtService: DebtService,
+    private val monthTotalAmountCalculatorService: MonthTotalAmountCalculatorService,
+    private val countryCurrencyDataStore: CountryCurrencyDataStore,
     @Assisted private val expenseGroupId: UUID,
     @Assisted private val prevExpense: Expense?
 ) : AbstractSavedStateViewModelFactory(owner, null) {
@@ -24,6 +30,9 @@ class AddEditExpenseViewModelFactory @AssistedInject constructor(
     ): T = AddEditExpenseViewModel(
         savedStateHandle = handle,
         expenseGroupId = expenseGroupId,
-        prevExpense = prevExpense
+        prevExpense = prevExpense,
+        debtService = debtService,
+        monthTotalAmountCalculatorService = monthTotalAmountCalculatorService,
+        countryCurrencyDataStore = countryCurrencyDataStore
     ) as T
 }
--- app/src/main/java/com/puzzle/industries/budgetplan/factory/viewModel/implementation/AddEditIncomeViewModelFactory.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/factory/viewModel/implementation/AddEditIncomeViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.savedstate.SavedStateRegistryOwner
 import com.puzzle.industries.budgetplan.viewModels.budget.income.AddEditIncomeViewModel
+import com.puzzle.industries.domain.datastores.CountryCurrencyDataStore
 import com.puzzle.industries.domain.models.income.Income
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -12,13 +13,18 @@ import dagger.assisted.AssistedInject
 @Suppress("UNCHECKED_CAST")
 class AddEditIncomeViewModelFactory @AssistedInject constructor(
     @Assisted owner: SavedStateRegistryOwner,
-    @Assisted private val prevIncome: Income?
+    @Assisted private val prevIncome: Income?,
+    private val countryCurrencyDataStore: CountryCurrencyDataStore
 ) : AbstractSavedStateViewModelFactory(owner, null) {
 
     override fun <T : ViewModel> create(
         key: String,
         modelClass: Class<T>,
         handle: SavedStateHandle
-    ): T = AddEditIncomeViewModel(savedStateHandle = handle, prevIncome = prevIncome) as T
+    ): T = AddEditIncomeViewModel(
+        savedStateHandle = handle,
+        prevIncome = prevIncome,
+        countryCurrencyDataStore = countryCurrencyDataStore
+    ) as T
 
 }
