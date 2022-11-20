784c4d49fd80382038332911c23c3667c1a4d76e
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-11-20 22:25:07 +0200

    added currency symbol state flow

--- app/src/main/java/com/puzzle/industries/budgetplan/viewModels/budget/income/AddEditIncomeViewModel.kt
+++ app/src/main/java/com/puzzle/industries/budgetplan/viewModels/budget/income/AddEditIncomeViewModel.kt
@@ -4,22 +4,28 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.puzzle.industries.budgetplan.delegates.AddEditItemStateHandlerDelegate
 import com.puzzle.industries.budgetplan.delegates.CoroutineHandlerDelegate
+import com.puzzle.industries.budgetplan.delegates.CurrencySymbolObserverDelegate
 import com.puzzle.industries.budgetplan.delegates.SavedStateHandlerDelegate
 import com.puzzle.industries.budgetplan.delegates.implementation.AddEditItemStateHandlerDelegateImpl
 import com.puzzle.industries.budgetplan.delegates.implementation.CoroutineHandlerDelegateImpl
+import com.puzzle.industries.budgetplan.delegates.implementation.CurrencySymbolObserverDelegateImpl
 import com.puzzle.industries.budgetplan.delegates.implementation.SavedStateHandlerDelegateImpl
 import com.puzzle.industries.budgetplan.util.configs.FrequencyConfig
+import com.puzzle.industries.domain.datastores.CountryCurrencyDataStore
 import com.puzzle.industries.domain.models.income.Income
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import java.util.*
 
 class AddEditIncomeViewModel @AssistedInject constructor(
     private val savedStateHandle: SavedStateHandle,
-    private val prevIncome: Income?
+    private val prevIncome: Income?,
+    private val countryCurrencyDataStore: CountryCurrencyDataStore
 ) : ViewModel(),
+    CurrencySymbolObserverDelegate by CurrencySymbolObserverDelegateImpl(countryCurrencyDataStore),
     SavedStateHandlerDelegate by SavedStateHandlerDelegateImpl(savedStateHandle),
     AddEditItemStateHandlerDelegate by AddEditItemStateHandlerDelegateImpl(savedStateHandle),
     CoroutineHandlerDelegate by CoroutineHandlerDelegateImpl() {
@@ -73,6 +79,8 @@ class AddEditIncomeViewModel @AssistedInject constructor(
     val crudActionReasonHandler by lazy {
         getActionReasonStateHandler()
     }
+
+    val currencySymbol: StateFlow<String> = currencySymbolFlow
 
     val income: Income
         get() = Income(
